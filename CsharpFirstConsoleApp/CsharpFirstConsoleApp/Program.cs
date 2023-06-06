a099dcdc688b531ad950c4c7ea68e1ecc26348a2
ShahnewazRajit <77947074+ShahnewazRajit@users.noreply.github.com>
2023-06-06 21:32:14 +0600

    Csharp First Console Making And Type

---
+++ CsharpFirstConsoleApp/CsharpFirstConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please Enter Your Name");
+            var takeName = System.Console.ReadLine();
+            System.Console.WriteLine("Hello"+takeName);
+            System.Console.WriteLine("--------------");
+            System.Console.WriteLine("Please Enter Your Password");
+            var password = System.Console.ReadLine();
+            System.Console.WriteLine("Welcome"+password);
+            System.Console.ReadKey();
+        }
+    }
+}
