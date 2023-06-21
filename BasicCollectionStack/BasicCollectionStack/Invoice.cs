06b74bd6ce5d90adc66128ac4f018782677f63ec
ShahnewazRajit <77947074+ShahnewazRajit@users.noreply.github.com>
2023-06-22 00:46:23 +0600

    Csharp Basic Collection And Stack Implementation

---
+++ BasicCollectionStack/BasicCollectionStack/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicCollections
+{
+    public class Invoice
+    {
+        public string? ProductName { get; set; }
+
+        public int Amount { get; set; }
+
+        public string? Email { get; set; }
+
+        public double Total { get; set; }
+
+        public Invoice(string productName, int amount, string email, double total)
+        {
+            ProductName = productName;
+            Amount = amount;
+            Email = email;
+            Total = total;
+
+        }
+    }
+}
