2a9459fde2c6a53fff0de2ba93c25f943e0b0b53
ShahnewazRajit <77947074+ShahnewazRajit@users.noreply.github.com>
2023-06-25 04:26:41 +0600

    Csharp Advance Interface implementation

---
+++ CsharpAdvance/BuildinInterface/TestIEnumerable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildinInterface
+{
+    internal class TestIEnumerable :IEnumerable<object>
+    {
+        public static void CollectionSum(IEnumerable<int> collection)
+        {
+            int sum = 0;
+
+            foreach(var item in collection)
+            {
+                sum += item;
+
+            }
+            Console.WriteLine("Sum is {0}",sum);
+        }
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CsharpAdvance/EventDelegate/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EventDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DelegateO.CheckDelegate();
+            DelegateO.DelegateExample();
+        }
+    }
+}
